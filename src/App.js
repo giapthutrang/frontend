d37fc564f7c4fb796eb75811ecf71021d34a78fb
giapthutrang <giapthutrang2891@gmail.com>
2020-08-28 23:56:22 +0700

    code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  TextField,
+  Typography,
+  Button,
+  Paper,
+} from '@material-ui/core'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  
+
+  function handleChangeUsername(event) {
+    setUsername(event.target.value)
+  }
+  
+  function handleChangePassword(event) {
+    setPassword(event.target.value)
+  }
+
+  const handleSignIn = () => {
+    console.log(username, password)
+    if(password.length<6) alert('password yếu');
+
+  }
+
+  const handleSignUp = () => {
+
+  }
+
+  //npm install @material-ui/core
+  const buttonStyle = {
+    color: 'lightcyan',
+    width: 200,
+    height: 50,
+    backroundColor: '#d32f2f',
+    margin: 10
+  }
+  function handleKeyPress (event) {
+    if (event.key=== 'enter') handleSignIn();
+  }
+  return <Paper style={{
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flexDirection: 'column',
+    height: 400,
+    alignItems: 'center',
+    flex: 1
+  }}>
+
+    <TextField 
+    label='Tài Khoản' 
+    type="text" 
+    onChange={handleChangeUsername}></TextField>
+
+    <TextField 
+    label='Mật Khẩu' 
+    type="password" 
+    onKeyPress={handleKeyPress}
+    onChange={handleChangePassword} />
+
+    <Button onClick={handleSignIn} variant="contained" style={buttonStyle}><Typography>dang nhap</Typography></Button>
+
+    <Button onClick={handleSignUp} variant="text" style={buttonStyle}><Typography>dang ki</Typography></Button>
+
+    <br></br>
+
+    <Typography>{username}</Typography>
+    <Typography>{password}</Typography>
+
+  </Paper>
 }
 
 export default App;
+//module.exports=app;
