486f6fea117239f6c536e562bf9f976f3b6aa4f7
giapthutrang <giapthutrang2891@gmail.com>
2020-08-30 13:50:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,77 +1,22 @@
 import React, { useState } from 'react';
-import {
-  TextField,
-  Typography,
-  Button,
-  Paper,
-} from '@material-ui/core'
+import HomePage from './views/HomePage/HomePage';
 
 function App(props) {
-
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [version, setVersion] = useState(1)
+  function hamxuli() {
+    alert('đây là hàm xử lí')
+  }
+  return <div>
+  <HomePage
+    params1='thien'
+    params2={hamxuli}
+    version={version}
+    setVersion={setVersion}
+  >
+    </HomePage>
+    <h1>{version}</h1>
+  </div>
   
-
-  function handleChangeUsername(event) {
-    setUsername(event.target.value)
-  }
-  
-  function handleChangePassword(event) {
-    setPassword(event.target.value)
-  }
-
-  const handleSignIn = () => {
-    console.log(username, password)
-    if(password.length<6) alert('password yếu');
-
-  }
-
-  const handleSignUp = () => {
-
-  }
-
-  //npm install @material-ui/core
-  const buttonStyle = {
-    color: 'lightcyan',
-    width: 200,
-    height: 50,
-    backroundColor: '#d32f2f',
-    margin: 10
-  }
-  function handleKeyPress (event) {
-    if (event.key=== 'enter') handleSignIn();
-  }
-  return <Paper style={{
-    display: 'flex',
-    justifyContent: 'flex-start',
-    flexDirection: 'column',
-    height: 400,
-    alignItems: 'center',
-    flex: 1
-  }}>
-
-    <TextField 
-    label='Tài Khoản' 
-    type="text" 
-    onChange={handleChangeUsername}></TextField>
-
-    <TextField 
-    label='Mật Khẩu' 
-    type="password" 
-    onKeyPress={handleKeyPress}
-    onChange={handleChangePassword} />
-
-    <Button onClick={handleSignIn} variant="contained" style={buttonStyle}><Typography>dang nhap</Typography></Button>
-
-    <Button onClick={handleSignUp} variant="text" style={buttonStyle}><Typography>dang ki</Typography></Button>
-
-    <br></br>
-
-    <Typography>{username}</Typography>
-    <Typography>{password}</Typography>
-
-  </Paper>
 }
 
 export default App;
-//module.exports=app;
